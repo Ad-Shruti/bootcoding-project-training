bfc0ce0ad7303de42c87ed8b9fd2d4ba92e5866c
shruti <chaurasiyashruti3@gmail.com>
2022-10-17 04:05:05 -0700

    add new array and arraylist exercise

---
+++ src/main/exercise/ArrayExercise.java
@@ -0,0 +1,33 @@
+package main.exercise;
+
+public class ArrayExercise {
+    public static void main(String[] args) {
+
+        // Array declare +initilize
+        int[] numbers = {10,20,30,40,50};
+
+        // Array declare
+        int[] ages = new int[5];
+
+        // Array initilization
+//        ages[0] = 10;
+//        ages[1] = 20;
+//        ages[2] = 30;
+//        ages[3] = 40;
+//        ages[4] = 50;
+
+        for (int i = 0; i< ages.length; i++){
+            ages[i] = 10 * (i+1);
+        }
+
+        String[] months =  {"jan","feb","mar"};
+
+        for (int i =0; i< months.length; i++)
+            System.out.println(months[i]);
+
+        // how to traverse array element
+        for (int i = 0; i<ages.length; i++){
+            System.out.println(i + "=" + ages[i]);
+        }
+    }
+}
